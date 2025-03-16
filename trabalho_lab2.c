2cc3831e3fddf9f9e0dac513284b9a3ebf44d10f
DiogoDDSantos <diogodalbianco064@gmail.com>
2025-03-16 17:41:00 -0300

    somente adicionei umas coisas

---
+++ trabalho_lab2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct{
+    int id;
+    char descricao[100];
+    char tipo[100];
+    char status[100];
+
+} Dispositivo;
+
+
+typedef struct{
+    char tipo[100];
+
+} Sensor;
+
+void insere_dispositivo(Dispositivo *dispositivo){
+
+}
+
+void remove_dispositivo(Dispositivo *dispositivo){
+
+}
+
+void atualizar_dispositivo(Dispositivo *dispositivo){
+
+}
+
+void buscar_dispositivo(Dispositivo *dispositivo){
+
+}
+
+void lista_de_dispositivos(Dispositivo *dispositivo){
+
+}
+
+int main(){
+
+
+
+    return 0;
+}
