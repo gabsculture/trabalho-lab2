78ff9c453c452665f3cd5b0ff3c1f521cfa7b9b0
DiogoDDSantos <diogodalbianco064@gmail.com>
2025-03-17 19:48:27 -0300

    corrigi os erros

--- trabalho_lab2.c
+++ trabalho_lab2.c
@@ -16,23 +16,23 @@ typedef struct{
 
 } Sensor;
 
-void insere_dispositivo(Dispositivo *dispositivo){
+void insere_dispositivo(){
 
 }
 
-void remove_dispositivo(Dispositivo *dispositivo){
+void remove_dispositivo(){
 
 }
 
-void atualizar_dispositivo(Dispositivo *dispositivo){
+void atualizar_dispositivo(){
 
 }
 
-void buscar_dispositivo(Dispositivo *dispositivo){
+void buscar_dispositivo(){
 
 }
 
-void lista_de_dispositivos(Dispositivo *dispositivo){
+void lista_de_dispositivos(){
 
 }
 
@@ -40,16 +40,16 @@ int main(){
     int opicao;
 
     printf("\nDigite qual das opcoes deseja fazer:");
-    printf("1 - Inserir dispositivo\n 2 - remover dispostivo\n 3 - Atualizar dispositivo\n 4 - Buscar dispositivo\n 5 - lista de dispositivos\n 6 - Sair\n Qual opcao deseja deseja? ");
-    scanf("%d",&opicao);
+    printf("\n1 - Inserir dispositivo\n 2 - remover dispostivo\n 3 - Atualizar dispositivo\n 4 - Buscar dispositivo\n 5 - lista de dispositivos\n 6 - Sair\n Qual opcao deseja deseja? ");
+    scanf("%d",&opicao); //le a opção do usuario
 
-    while(opicao > 6 && opicao < 1){
+    while(opicao > 6 || opicao < 1){ //verifica se foi digitado uma opação valida
       printf("\nDigite uma opcao valida:");
       printf("1 - Inserir dispositivo\n 2 - remover dispostivo\n 3 - Atualizar dispositivo\n 4 - Buscar dispositivo\n 5 - lista de dispositivos\n 6 - Sair\n Qual opcao deseja deseja? ");
       scanf("%d",&opicao);
     }
 
-    do{
+    do{ //Direciona para as funções
         switch(opicao){
         case 1:
           insere_dispositivo();
@@ -58,16 +58,27 @@ int main(){
           remove_dispositivo();
           break;
         case 3:
-          atuailar_dispositivos();
+          atualizar_dispositivo();
           break;
         case 4:
-          buscar_dispositivos();
+          buscar_dispositivo();
           break;
         case 5:
           lista_de_dispositivos();
           break;
         default:
           break;
+      }
+
+      //após a verificação pede para o usuario escolher a proxima opção
+      printf("\nDigite qual das opcoes deseja fazer:");
+      printf("\n1 - Inserir dispositivo\n 2 - remover dispostivo\n 3 - Atualizar dispositivo\n 4 - Buscar dispositivo\n 5 - lista de dispositivos\n 6 - Sair\n Qual opcao deseja deseja? ");
+      scanf("%d",&opicao);
+
+      while(opicao > 6 || opicao < 1){
+        printf("\nDigite uma opcao valida:");
+        printf("1 - Inserir dispositivo\n 2 - remover dispostivo\n 3 - Atualizar dispositivo\n 4 - Buscar dispositivo\n 5 - lista de dispositivos\n 6 - Sair\n Qual opcao deseja deseja? ");
+        scanf("%d",&opicao);
       }
     }while(opicao != 6);
 
