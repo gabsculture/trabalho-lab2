bab5122c44eeb1aa45a386f9fad2783d0d42715d
Gabriela Vitória da Rosa Soares <85495852+gabsculture@users.noreply.github.com>
2025-06-01 23:02:19 -0300

    Create Sensores.h

---
+++ Sensores.h
@@ -0,0 +1,14 @@
+#ifndef SENSORES_H
+#define SENSORES_H
+
+#include "estruturas.h"
+
+Sensor* criar_sensor(int id, char* tipo, char* subtipo, float valor);
+void insere_sensor(Dispositivo* dispositivo, Sensor* novo);
+void remove_sensor(Dispositivo* dispositivo, int id);
+void listar_sensores(Dispositivo* dispositivo);
+void tipo_validos(char* tipo_sensor);
+void subtipo_validos(char* subtipo_sensor);
+Dispositivo* opera_sensores(Dispositivo* lista);
+
+#endif
