967cf45f05f38f25c78ace8cd9a85842c0a6d144
Gabriela Vitória da Rosa Soares <85495852+gabsculture@users.noreply.github.com>
2025-06-01 23:01:18 -0300

    Update Dispositivos.h

--- Dispositivos.h
+++ Dispositivos.h
@@ -2,9 +2,30 @@
 #define DISPOSITIVOS_H
 
 #include "estruturas.h"
+#include <stdbool.h>
 
-Dispositivo* inicializa();
-Dispositivo* opera_dispositivos(Dispositivo* lista);
-void libera(Dispositivo* lista);
+Dispositivo* inicializa(void);
+
+Dispositivo* criar_dispositivo(int id, char* descricao, char* tipo, char* status);
+
+void insere_dispositivo(Dispositivo** lista, Dispositivo* novo);
+
+void remove_dispositivo(Dispositivo** lista, int id);
+
+bool valida_id(Dispositivo* lista, int id);
+
+void busca(Dispositivo* dispositivo);
+
+void listar_dispositivos(Dispositivo* lista);
+
+void minusculas(char* str);
+
+void confere_dispositivos(char* tipo);
+
+void condicoes_validas(char* status);
+
+void libera(Dispositivo* lst);
+
+Dispositivo* opera_dispositivos(Dispositivo* dispositivo);
 
 #endif
