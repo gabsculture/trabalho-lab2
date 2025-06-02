8dcf9cc9d47d23fdb3cfc325dccff41926b46348
Gabriela Vitória da Rosa Soares <85495852+gabsculture@users.noreply.github.com>
2025-06-01 23:03:21 -0300

    Create Funcoes.h

---
+++ Funcoes.h
@@ -0,0 +1,21 @@
+#ifndef FUNCOES_H
+#define FUNCOES_H
+
+#include "estruturas.h"
+#include <stdbool.h>
+
+
+extern FILE *arquivoCsv;
+
+void minusculas(char* str);  
+void pegaTempo(char *buffer, int tamanho);
+void incluirTimeStamp(Sensor* sensor, float valor);
+
+int compararCrescente(const void *a, const void *b);
+int compararDecrescente(const void *a, const void *b);
+
+bool verificacsv(Sensor* sensor);
+void exportacsv(Sensor* sensor, Dispositivo* dispositivo, int contador);
+void importacsv(Sensor* sensor, Dispositivo* dispositivo);
+
+#endif
