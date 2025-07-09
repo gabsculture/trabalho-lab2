6c77bb451d2878c803ff20e14ae828666a492bfd
Gabriela Vitória da Rosa Soares <85495852+gabsculture@users.noreply.github.com>
2025-07-09 14:49:53 -0300

    Update Arvore.c

--- Arvore.c
+++ Arvore.c
@@ -1,1 +1,20 @@
+#include "estruturas.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
 
+NoArvore* plantar_arvore(Valor** vetor, int inicio, int fim) {
+    if (inicio > fim) return NULL;
+
+    int meio = (inicio + fim) / 2;
+
+    NoArvore* no = malloc(sizeof(NoArvore));
+    strcpy(no->timestamp, vetor[meio]->timestamp);
+    no->valor = vetor[meio]->valor;
+    no->esquerda = plantar_arvore(vetor, inicio, meio - 1);
+    no->direita = plantar_arvore(vetor, meio + 1, fim);
+
+    return no;
+}
