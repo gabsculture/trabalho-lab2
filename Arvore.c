2bede88c686a6e407a72b30fefece84fdf225487
Gabriela Vitória da Rosa Soares <85495852+gabsculture@users.noreply.github.com>
2025-07-09 15:15:36 -0300

    converter listta pra vetor

--- Arvore.c
+++ Arvore.c
@@ -18,9 +18,27 @@ NoArvore* plantar_arvore(Valor** vetor, int inicio, int fim) {
 
     return no;
 }
+
 void desmatar_arvore(NoArvore* raiz) {
     if (raiz == NULL) return;
     desmatar_arvore(raiz->esquerda);
     desmatar_arvore(raiz->direita);
     free(raiz);
+}
+
+int contar_valores(Valor* lista) {
+    int contador = 0;
+    while (lista) {
+        contador++;
+        lista = lista->proximo;
+    }
+    return contador;
+}
+
+void lista_para_vetor(Valor* lista, Valor** vetor) {
+    int i = 0;
+    while (lista) {
+        vetor[i++] = lista;
+        lista = lista->proximo;
+    }
 }
